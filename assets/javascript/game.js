dca547a5520ec070fa0c151696e52e894401abd6
jjlarkin <jjlarkin85@gmail.com>
2017-03-03 18:59:04 -0500

    second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,92 @@
 /**************************************
-Attempt at Hangman
+ Attempt at Hangman
 
-James Larkin
+ James Larkin
 
-***************************************/
+ ***************************************/
 
-/* Things we need our program to do:
-1.1. Pick a random word. 
---On key randomly pick from Array of words
+/* Psuedocode
+ 1.1. Pick a random word.
+ --On key randomly pick from Array of words
 
-1.2. Display amount of letters in the word via dashes 
-use index to creat the desired number of dashes
+ 1.2. Display amount of letters in the word via dashes
+ use index to creat the desired number of dashes
 
-2.1. Tell player to pick a letter/
+ 2.1. Tell player to pick a letter/
 
-2.2 Determine if the chosen letter is in the word
+ 2.2 Determine if the chosen letter is in the word
 
-2.3 Place the letter in the correct place if correct/subtract from total tries if wrong 
+ 2.3 Place the letter in the correct place if correct/subtract from total tries if wrong
 
-3. Display scoreboard
+ 3. Display scoreboard
 
-4. Tell player they won/lost and reset the game
-*/
+ 4. Tell player they won/lost and reset the game
+ */
 
 
 
 
 function chooseWord(randoWord) {
-    var wordList = ["red", "party", "supply"];
-    var randoWord = wordList[Math.floor(Math.random() * wordList.length)];
+    var wordList = ["red", "party", "supply", "cheese", "quadrant", "another", "word"];
+    randoWord = wordList[Math.floor(Math.random() * wordList.length)];
     console.log(randoWord)
     return randoWord;
 
 };
 //count characters and write blanks from wordList[]
 function blanks(inWord) {
+    var blnkArr=[];
     for (i = 0; i < inWord.length; i++) {
-        console.log("_")
-    }
+
+        blnkArr.push("_");
+    };
+    console.log(blnkArr)
+    return blnkArr;
 };
 
-function isALetter(inCurrent) {
+function isALetter(inCurrent, inUserGuess, inArr, letterTrue) {
+    var inArr;
     for (i = 0; i < inCurrent.length; i++) {
-        if (charAt(i) === userGuess) {}
+        if (inCurrent.charAt(i) === inUserGuess) {
+            inArr[i]=inUserGuess
+        }
     }
+    console.log(inArr);
 };
 
 //********************************************************
 
 
 // Start up the game by pressing any key
-var wordlist = ["red", "party", "supply"];
-var wins, losses, guessesLeft, currentWord;
+
+var wins, losses, guessesLeft, currentWord, currentBlanks;
 
 
 //
 console.log("Press any letter to start")
 document.onkeyup = function(event) {
 
-    var userGuess = String.fromCharCode(event.keyCode)
+    var userGuess = String.fromCharCode(event.keyCode);
+    currentWord = chooseWord(currentWord);
+    currentBlanks=blanks(currentWord);
 
 
-    if (typeof(userGuess) === "string") {
-
-        currentWord = chooseWord(currentWord);
-        blanks(currentWord);
-
-        userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-        console.log(userGuess);
-
-
-
-    }
-}
+    guessesLeft=0;
+    console.log(guessesLeft)
+    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 
 
 
-// Here we create the HTML that will be injected into our div and displayed on the page.
+    isALetter(currentWord, userGuess, currentBlanks);
+
+    console.log(userGuess);
+
+
+
+
+
+};
+
+
+
